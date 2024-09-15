6448bc6a41bef19bbc2db702a7d02437eeaaf2da
VBrawl <konstantosjim@gmail.com>
2024-09-15 19:09:37 +0300

    Modified trash_item to contain different info for different operating systems

--- src/trash_manager/trash_item.py
+++ src/trash_manager/trash_item.py
@@ -1,7 +1,16 @@
 import datetime
+import platform
 
 
-class TrashItem:
-    def __init__(self, original_path: str, deletion_date: str):
-        self.original_path = ''
-        self.deletion_date = datetime.datetime(1, 1, 1, 0, 0, 0)
+class ITrashItem:
+    def __init__(self, original_path: str, deletion_date: datetime.datetime):
+        self.original_path = original_path
+        self.deletion_date = deletion_date
+
+
+if platform.system() == "Linux":
+    class TrashItem(ITrashItem):
+        def __init__(self, original_path: str, deletion_date: datetime.datetime, trashed_path: str, trashinfo_path: str):
+            ITrashItem.__init__(self, original_path, deletion_date)
+            self.trashed_path = trashed_path
+            self.trashinfo_path = trashinfo_path
