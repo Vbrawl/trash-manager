9b744e656c64a7ff66a33875574cf6022b1b7c52
VBrawl <konstantosjim@gmail.com>
2024-09-15 21:45:58 +0300

    Added for_trashed_file alternative constructor

--- src/trash_manager/trash_item.py
+++ src/trash_manager/trash_item.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 import datetime
 import platform
+from pathlib import Path
 
 
 class ITrashItem:
@@ -14,3 +16,39 @@ if platform.system() == "Linux":
             ITrashItem.__init__(self, original_path, deletion_date)
             self.trashed_path = trashed_path
             self.trashinfo_path = trashinfo_path
+
+        @staticmethod
+        def for_trashed_file(path: str) -> TrashItem:
+            def find_tag(start: int, tag: str):
+                pos = content.find(tag, start)
+                if pos == -1:
+                    return -1, 0
+                end_pos = content.find(b'\n', pos)
+                if end_pos == -1:
+                    return -1, 0
+                return pos + len(tag), end_pos
+
+            trash_info_tag = b"[Trash Info]"
+            path_tag = b"Path="
+            deletion_date_tag = b"DeletionDate="
+
+            path = Path(path)
+            trashinfo_path = path.parent.parent / "info" / (path.name + ".trashinfo")
+            with open(str(trashinfo_path), 'rb') as f:
+                content = f.read()
+
+            trash_info_pos = content.find(trash_info_tag)
+            if trash_info_pos == -1:
+                return None
+
+            path_pos, path_end_pos = find_tag(trash_info_pos, path_tag)
+            if path_pos == -1:
+                return None
+
+            deletion_date_pos, deletion_date_end_pos = find_tag(trash_info_pos, deletion_date_tag)
+            if deletion_date_pos == -1:
+                return None
+
+            path_value = content[path_pos:path_end_pos].decode()
+            deletion_date_value = datetime.datetime.fromisoformat(content[deletion_date_pos:deletion_date_end_pos].decode())
+            return TrashItem(path_value, deletion_date_value, str(path), str(trashinfo_path))
---
+++ tests/test_trash_item.py
@@ -0,0 +1,40 @@
+import pytest
+import platform
+import datetime
+
+import os
+
+from trash_manager import TrashItem
+
+
+@pytest.mark.skipif(platform.system() != "Linux", reason="Function specific to linux")
+def test__LINUX__TrashItem_from_fileinfo(tmp_path):
+    # Prepare fake data
+    original_filepath = "/tmp/my_file"
+    deletion_date = "2024-08-07T01:53:48"
+    data = f"""
+[Trash Info]
+Path={original_filepath}
+DeletionDate={deletion_date}
+""".encode()
+
+    # Prepare fake trash
+    os.mkdir(os.path.join(tmp_path, "Trash"))
+    os.mkdir(os.path.join(tmp_path, "Trash", "files"))
+    os.mkdir(os.path.join(tmp_path, "Trash", "info"))
+
+    # Create a fake trashed file
+    trashed_filepath = os.path.join(tmp_path, "Trash", "files", "from_fileinfo")
+    open(trashed_filepath, 'w').close()
+
+    # Create a fake trashinfo file
+    trashinfo_filepath = os.path.join(tmp_path, "Trash", "info", "from_fileinfo.trashinfo")
+    with open(trashinfo_filepath, 'wb') as f:
+        f.write(data)
+
+    # Test the alternative constructor
+    ti = TrashItem.for_trashed_file(trashed_filepath)
+    assert ti.trashinfo_path == trashinfo_filepath
+    assert ti.trashed_path == trashed_filepath
+    assert ti.original_path == original_filepath
+    assert ti.deletion_date == datetime.datetime.fromisoformat(deletion_date)
